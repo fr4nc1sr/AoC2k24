a7e1204afd5a0135b47ab67e5bccfe44eac71819
Your Name <you@example.com>
2024-12-01 18:36:47 +0100

    giorno 1 risolto

--- g1/g1.py
+++ g1/g1.py
@@ -1,3 +1,4 @@
+
 def solve1(f):
     #estrarre dati dal file
     righe = [list(map(int, line.split()))for line in f]
@@ -12,6 +13,25 @@ def solve1(f):
     somma = sum(abs(ele2-ele1) for ele1, ele2 in zip(colonne[0],colonne[1]))
     return somma
 
+def solve2(f):
+        #estrarre dati dal file
+    righe = [list(map(int, line.split()))for line in f]
+    colonne = list(zip(*righe))
+    colonne = [list(col) for col in colonne]
+
+    #ordinarli
+    for col in colonne:
+        col.sort()
+
+    somma = 0
+    for num in colonne[0]:
+        # Contare quante volte 'num' appare in colonne[1]
+        count = colonne[1].count(num)
+        # Aggiungere num * count al punteggio
+        somma += num * count
+
+    return somma
 
 with open("input.txt","r") as f:
-    print(solve1(f))
+    #print(solve1(f))
+    print(solve2(f))
